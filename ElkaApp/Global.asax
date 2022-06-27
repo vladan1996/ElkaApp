aa3bca82b8924211a7abf0640446d01a4f78010a
ranko <ranko.kremenovic@axelyos.com>
2022-06-27 15:09:17 +0200

    Add project files.

---
+++ ElkaApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElkaApp.MvcApplication" Language="C#" %>
---
+++ ElkaApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElkaApp.Startup))]
+namespace ElkaApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
