126de1d613769c29c76ef6d8d55c3ad33a4d8c8d
Jelena <jelena.pivac96@gmail.com>
2022-08-03 09:25:41 +0200

    Fix image

--- ElkaApp/PanelLogic.cs
+++ ElkaApp/PanelLogic.cs
@@ -172,7 +172,7 @@ namespace ElkaApp
             var obj = DB.Users.FirstOrDefault(x => x.UserID == id);
             if(obj.FilePath == null)
             {
-                obj.FilePath = "https://upload.wikimedia.org/wikipedia/commons/thum-b/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
+                obj.FilePath = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
             }
             return obj;
         }
